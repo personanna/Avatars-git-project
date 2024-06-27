fd63e0310f261c3d387c977fb92487243ed6a9d3
personanna <shavranya@gmail.com>
2024-06-26 20:26:21 -0500

    New pushing

--- src/test/java/org/example/Testing.java
+++ src/test/java/org/example/Testing.java
@@ -2,7 +2,9 @@ package org.example;
 
 public class Testing {
     public static void main(String[] args) {
-        System.out.println("My first git class");
+        System.out.println("Hello world");
+        System.out.println("I am having the time of my life");
+        System.out.println("I cannot wait to get a job");
 
     }
 }
